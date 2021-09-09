b134fbbd2897b4f9a3b0c6e6ae2d8a86bb926909
sophie <justanothersophie@gmail.com>
2021-09-09 14:56:30 -0400

    Completed lab, all tests passing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+artists = Artist.create([{ name: "Adele" }, { name: "Rilo Kiley" }])
+Song.create(title: "Teenage Love Song", artist: artists.last)
